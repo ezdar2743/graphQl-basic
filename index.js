ed7cfcbcf839f7db640dcc3e8ece7fefdc9e0e46
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-30 16:33:40 +0900

    server

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("sss");
+import { createServer } from "@graphql-yoga/node";
+import { version } from "graphql";
+const server = new createServer({});
+
+server.start(() => console.log("start"));
